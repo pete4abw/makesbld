ba39c6ab5460d529bab58f53ba9205652e7ba801
Peter Hyman <pete@peterhyman.com>
2018-04-05 14:13:19 -0500

    remove \ chars in doinst

--- install/doinst.sh
+++ install/doinst.sh
@@ -3,13 +3,13 @@
 # trick for renaming new and old files borrowed from Slackware
 # installation scripts by P. Volkerding
 config() {
-    NEW="\$1"
-    OLD="\`dirname \$NEW\`/\`basename \$NEW .new\`"
+    NEW="$1"
+    OLD="`dirname $NEW`/`basename $NEW .new`"
     # If there's no config file by that name, mv it over:
-    if [ ! -r \$OLD ]; then
-        mv \$NEW \$OLD
-    elif [ "\`cat \$OLD | md5sum\`" = "\`cat \$NEW | md5sum\`" ]; then # toss the redundant copy
-        rm \$NEW
+    if [ ! -r $OLD ]; then
+        mv $NEW $OLD
+    elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
+        rm $NEW
     fi
     # Otherwise, we leave the .new copy for the admin to consider...
 }
